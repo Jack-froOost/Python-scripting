3f010aea86a1fbba413211e38841f767f3fa9c2d
Jack-froOost <mhnd003@gmail.com>
2025-08-14 14:33:13 +0200

    Convert submodule to regular folder

---
+++ clipboard image saver/imageClipBoardTest.py
@@ -0,0 +1,10 @@
+from PIL import ImageGrab, Image
+import os
+
+#testing the ImageGrap functionality, also my first git usage !
+
+img = ImageGrab.grabclipboard() #if the last thing you copied is an image, it returns a PIL of the image
+#if it's directories, it returns a list of the paths in string format.
+print(img)
+
+print(type(img))
